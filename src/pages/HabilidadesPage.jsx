d5dc93430b363effa0d0f02d448a3a1e8fc916a2
Alex Daniel <1151372@senati.pe>
2023-07-15 19:06:22 -0500

    Creando la pages

---
+++ src/pages/HabilidadesPage.jsx
@@ -0,0 +1,6 @@
+
+export const HabilidadesPage = () => {
+  return (
+    <div>HabilidadesPage</div>
+  )
+}
---
+++ src/pages/LinuxPage.jsx
@@ -0,0 +1,6 @@
+
+export const LinuxPage = () => {
+  return (
+    <div>LinuxPage</div>
+  )
+}
