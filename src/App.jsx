f7b3a5e5765432e1f757696e54d353aa094eff5a
Alex Daniel <1151372@senati.pe>
2023-07-15 19:09:32 -0500

    Creando las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
 
 // FUENTE
 import '@fontsource/raleway/300.css';
 import '@fontsource/raleway/400.css';
 import '@fontsource/raleway/500.css';
 import '@fontsource/raleway/700.css';
-import { Typography } from '@mui/material';
+import { Navbar } from './UI/Navbar';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { InicioPage } from './pages/InicioPage';
+import { AboutPage } from './pages/AboutPage';
+import { HabilidadesPage } from './pages/HabilidadesPage';
+import { LinuxPage } from './pages/LinuxPage';
+import { ContactPage } from './pages/ContactPage';
 
 
 
 export const App = () => {
   return (
-    <Typography variant='h1'>
-      hello world!
-    </Typography>
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<InicioPage/> } />
+      <Route path='/about' element={<AboutPage/> } />
+      <Route path='/skills' element={<HabilidadesPage/> } />
+      <Route path='/linux' element={<LinuxPage/> } />
+      <Route path='/contact' element={<ContactPage/> } />
+      <Route path='/*' element={ <Navigate to="/" />  } />
+    </Routes>
+    </>
   )
 }
