32c623118a24480d251c6bfe245e0cf13dce3a21
Alex Daniel <1151372@senati.pe>
2023-07-15 19:38:50 -0500

    Acabando navbar

--- src/UI/Navbar.jsx
+++ src/UI/Navbar.jsx
@@ -1,16 +1,22 @@
 import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import React from "react";
+import { NavLink } from "react-router-dom";
+import  './nav.css'
 
 export const Navbar = () => {
   return (
-    <AppBar position="static">
-      <Toolbar component='nav'  >
-      <Typography variant="h5" sx={{fontStyle:'italic'}}>
-        Alex Daniel
-      </Typography>
-      <Box>
-
-      </Box>
+    <AppBar position="static" color="transparent" sx={{justifyContent:'center'}}  >
+      <Toolbar component="nav">
+        <Typography variant="h5" color='primary.main' sx={{ fontStyle: "italic", flexGrow: 1, fontWeight:700 }}>
+          Alex Daniel
+        </Typography>
+        <Box>
+          <NavLink to='/' >Inicio</NavLink>
+          <NavLink to='/about' >Sobre mí</NavLink>
+          <NavLink to='/skills' >Habilidades</NavLink>
+          <NavLink to='/linux' >Linux</NavLink>
+          <NavLink to='/contact' >Contacto</NavLink>
+        </Box>
       </Toolbar>
     </AppBar>
   );
