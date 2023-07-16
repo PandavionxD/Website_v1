e1705071b384a91f08003360428c8f0901fcee91
Alex Daniel <1151372@senati.pe>
2023-07-16 11:16:45 -0500

    Temrinando animaciones

--- src/pages/InicioPage.jsx
+++ src/pages/InicioPage.jsx
@@ -1,64 +1,83 @@
-import { Box, Button, Grid, Typography } from "@mui/material"
-import { useNavigate } from "react-router-dom"
-import { styled } from "styled-components"
-
-
+import { Box, Button, Grid, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { styled } from "styled-components";
 
 export const InicioPage = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const Image = styled("img")({
-    width:'90%',
-    objectFit:'contain',
-    objectPosition:'center'
-  })
-  
-  const NavNext = () =>{
-    navigate('/about',{
-      replace:false
-    })
+    width: "90%",
+    objectFit: "contain",
+    objectPosition: "center",
+  });
 
-  }
-  
+  const NavNext = () => {
+    navigate("/about", {
+      replace: false,
+    });
+  };
+
   return (
-    <Grid container spacing={3} justifyContent='center' alignItems='center' minHeight='90vh' >
-      <Grid item xs={12} sm={7} >
+    <Grid
+      container
+      spacing={3}
+      justifyContent="center"
+      alignItems="center"
+      minHeight="90vh"
+    >
+      <Grid  className="animate__animated animate__fadeInLeft"  item xs={12} sm={7}>
         {/* <Image src="../assets/Desarrollo.svg" alt="Desarrollo-web" /> */}
-        <Image src='/assets/Desarrollo.svg' alt="Desarrollo-web" />
+        <Image src="/assets/Desarrollo.svg" alt="Desarrollo-web" />
       </Grid>
-      <Grid item  xs={12} sm={5}  >
-        <Typography  variant="h3" component='h1' mb={2} sx={{
-          fontSize:{
-            xs:'2rem',
-            sm:'2.4rem',
-            md:'3rem'
-          },
-          textAlign:{
-            xs:'center',
-            sm:'left'
-          }
-        }} >
+      <Grid item xs={12} sm={5} className="animate__animated animate__fadeInRight">
+        <Typography
+          variant="h3"
+          component="h1"
+          mb={2}
+          sx={{
+            fontSize: {
+              xs: "2rem",
+              sm: "2.4rem",
+              md: "3rem",
+            },
+            textAlign: {
+              xs: "center",
+              sm: "left",
+            },
+          }}
+        >
           Bienvenido a mi Pagina Web!
         </Typography>
-        <Typography variant="body2" mb={2} sx={{
-          textAlign:'justify',
-          fontSize:{
-            sx:12,
-            sm:14
-          }
-        }} >
-          Aquí dare un breve descripción  acerca de mí, sobre mis hobbies y sobre cosas que me gustan y me apasionan,
-          Me considero una persona autodidacta, me encanta aprender por mí mismo y claro esta me encanta la informática. 
+        <Typography
+          variant="body2"
+          mb={2}
+          sx={{
+            textAlign: "justify",
+            fontSize: {
+              sx: 12,
+              sm: 14,
+            },
+          }}
+        >
+          Aquí dare un breve descripción acerca de mí, sobre mis hobbies y sobre
+          cosas que me gustan y me apasionan, Me considero una persona
+          autodidacta, me encanta aprender por mí mismo y claro esta me encanta
+          la informática.
         </Typography>
-        <Grid container xs={12} md={6} >
-        <Button  sx={{
-          width:'100%'
-        }} size="large" type="button" variant="contained" onClick={NavNext} >
-          Ver más
-        </Button>
-          </Grid>
+        <Grid container xs={12} md={4}>
+          <Button
+            sx={{
+              width: "100%",
+            }}
+            size="large"
+            type="button"
+            variant="contained"
+            onClick={NavNext}
+          >
+            Ver más
+          </Button>
+        </Grid>
       </Grid>
     </Grid>
-  )
-}
+  );
+};
